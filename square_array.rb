7ac29625c6b5ccb1d5774858e44b081bc04fa092
Paul Leugemors <pleugemors@yahoo.com>
2019-12-09 12:31:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,10 @@
-def square_array(array)
+#def square_array(array)
   # your code here
   square = []
   array.each {|num| square << num * num}
   return square
+#end
+
+def square_array(array)
+  array.collect {|num| num*num}
 end
